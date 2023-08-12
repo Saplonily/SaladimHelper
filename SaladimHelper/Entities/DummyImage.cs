f803d6ea41a5ad61007f45fc6abe9543ce64634a
Saplonily <Saplonily@outlook.com>
2023-08-12 22:47:29 +0800

    add dummy image

---
+++ SaladimHelper/Entities/DummyImage.cs
@@ -0,0 +1,22 @@
+﻿using Celeste.Mod.Entities;
+
+namespace Celeste.Mod.SaladimHelper.Entities;
+
+// yes, just these, cuz it's dummy!
+[CustomEntity($"{ModuleName}/DummyImage")]
+public class DummyImage : Entity
+{
+    public DummyImage(EntityData data, Vector2 offset)
+        : this(data.Position + offset, data.Width, data.Height, data.Attr("texture"))
+    {
+
+    }
+
+    public DummyImage(Vector2 position, float width, float height, string gpTex)
+    {
+        Position = position;
+        Collider = new Hitbox(width, height);
+
+        Add(new Image(GFX.Game[gpTex]));
+    }
+}
