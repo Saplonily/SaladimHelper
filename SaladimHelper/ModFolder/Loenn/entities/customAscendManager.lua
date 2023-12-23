84f8483c12409af109eaf9862b9a9f53e667cca8
Saplonily <Saplonily@outlook.com>
2023-12-23 15:10:05 +0800

    sorting, copied something from somewhere

---
+++ SaladimHelper/ModFolder/Loenn/entities/customAscendManager.lua
@@ -0,0 +1,17 @@
+local entity = {}
+entity.name = "SaladimHelper/CustomAscendManager"
+entity.depth = 0
+entity.texture = "@Internal@/summit_background_manager"
+entity.placements = {
+    name = "normal",
+    data = {
+        backgroundColor = "75a0ab",
+        streakColors = "ffffff,e69ecb",
+        introLaunch = false,
+        finalLaunch = false, 
+        borders = true,
+        dark = false
+    }
+}
+
+return entity
