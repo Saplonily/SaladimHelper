e7283edd069c6c8901acbfef3914e244b4fa3007
Saplonily <Saplonily@outlook.com>
2023-12-26 23:23:32 +0800

    update script

--- publishToZip.ps1
+++ publishToZip.ps1
@@ -1,2 +1,3 @@
 dotnet build -c Release
-Compress-Archive SaladimHelper/ModFolder/*,Documentation.md SaladimHelper.zip 
+$sh_v = ((Get-Content -Path SaladimHelper/ModFolder/everest.yaml -TotalCount 2)[1]).SubString(11)
+Compress-Archive SaladimHelper/ModFolder/*,Documentation.md "SaladimHelper v$sh_v.zip"
