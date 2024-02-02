5cace439a6f651bb56e665ab760d637ef2211144
Saplonily <Saplonily@outlook.com>
2024-02-02 13:21:59 +0800

    update publishToZip.ps1

--- publishToZip.ps1
+++ publishToZip.ps1
@@ -1,4 +1,4 @@
 dotnet build -c Release
 $sh_v_str = Get-Content -Path SaladimHelper/ModFolder/everest.yaml -Raw
 $sh_v = [regex]::Match($sh_v_str, "(?<=Version:\s)(.*?)\n").Value.Trim()
-Compress-Archive SaladimHelper/ModFolder/*,Documentation.md "SaladimHelper v$sh_v.zip"
+Compress-Archive SaladimHelper/ModFolder/*,Documentation.md "SaladimHelper v$sh_v.zip" -Force
