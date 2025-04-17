59303ee3df79b6f46d87854787df4b0533d8fcd2
Gamation <52021489+GamationOnGithub@users.noreply.github.com>
2025-04-17 12:59:36 -0700

    Add mid-boost dialogue to custom ascend manager

--- SaladimHelper/ModFolder/Loenn/entities/customAscendManager.lua
+++ SaladimHelper/ModFolder/Loenn/entities/customAscendManager.lua
@@ -1,16 +1,24 @@
 local entity = {}
+
 entity.name = "SaladimHelper/CustomAscendManager"
 entity.depth = 0
 entity.texture = "@Internal@/summit_background_manager"
+entity.fieldInformation = {
+	backgroundColor = {
+        fieldType = "color"
+    }
+}
+
 entity.placements = {
     name = "normal",
     data = {
-        backgroundColor = "75a0ab",
-        streakColors = "ffffff,e69ecb",
-        introLaunch = false,
-        finalLaunch = false, 
-        borders = true,
-        dark = false
+        backgroundColor = "75A0AB",
+        streakColors = "FFFFFF,E69ECB",
+	dialog = "",
+	introLaunch = false,
+	finalLaunch = false,
+	borders = true,
+	dark = false
     }
 }
 
